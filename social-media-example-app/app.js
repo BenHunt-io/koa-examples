5a459eee2968511712f0a7f270e31ca966abea41
ben <me@benhunt.io>
2023-09-19 12:20:56 -0500

    Create and Get mock Users in-memory

--- social-media-example-app/app.js
+++ social-media-example-app/app.js
@@ -1,13 +1,15 @@
 import Koa from 'koa';
 import Router from 'koa-router';
 import BodyParser from 'koa-bodyparser';
-import {createUser} from './src/controller/UserController.js'
+import {createUser, getUser} from './src/controller/UserController.js'
 
 const app = new Koa();
 const router = new Router();
 const bodyParser = new BodyParser();
 
 router.post("/user", createUser)
+router.get("/user/:id", getUser)
+
 
 app
   .use(bodyParser)
--- social-media-example-app/src/controller/UserController.js
+++ social-media-example-app/src/controller/UserController.js
@@ -1,14 +1,26 @@
-
-
 let lastId = 0;
-const users = [];
+let users = [];
 
 async function createUser(ctx, next){
+
     let user = ctx.request.body;
     user.id = ++lastId;
-    users.push[user];
+    users.push(user)
     ctx.response.body = user;
     next();
 }
 
-export {createUser}
+async function getUser(ctx, next){
+
+    let user = users.find(user => user.id == ctx.params.id);
+
+    if(!user){
+        ctx.throw(404, `User with id: ${ctx.params.id} not found`)
+    }
+
+    ctx.response.body = user;
+
+    next();
+}
+
+export {createUser, getUser}
